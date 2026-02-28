d11dda392ba1fa4b46249493f202bcc5bdf3c8cc
nawfallodhi <nawfal.lodhi@gmail.com>
2026-02-27 16:16:06 -0800

    Added Order Calculator + Product Descriptions

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -3,13 +3,7 @@ import { redirect } from 'next/navigation';
 import ContactPage from './contactClient';
 
 const page = async () => {
-  const session = await getServerSession();
-  const user = session?.user;
-
-    if(user){
-      redirect("/account")
-    }
-
+  
   return <ContactPage />
 }
 
