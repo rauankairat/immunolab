1b821bb67b6534fdc291b3d6aa0efd4a8344b12f
Rauan Kairatx <sengrauan@gmail.com>
2026-03-11 01:29:17 -0700

    fix

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -1,9 +1,11 @@
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 import { sendEmail } from "./email";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
